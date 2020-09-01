247182427029a6a558e32fca4ebb63ba4e8b112b
Erica <edkjones@gmail.com>
2020-09-01 17:03:59 -0400

    assignment18

---
+++ scripts/helper.js
@@ -0,0 +1,6 @@
+class Helper {
+  playPauseAndUpdate (song){
+    const duration = player.getDuration();
+    player.playPause(song);
+  }}
+var helper = newHelper;
--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,6 +1,6 @@
 {
   $('button#play-pause').on('click', function() {
-    player.playPause();
+    helper.playPauseAndUpdate();
     $(this).attr('playState'. player.playState);
   });
 
@@ -12,8 +12,26 @@
     if (nextSongIndex >= album.songs.length) {return;}
 
     const nextSong = album.songs[nextSongIndex];
-    player.playPause(nextSong);
+    helper.playPauseAndUpdate(nextSong);
   });
+
+$('#time-control input').on('input', function (event) {
+  player.skipTo(event.target.value);
+});
+
+$('#volume-control input').on('input', function (event){
+  player.setVolume(event.target.value);
+})
+
+  setInterval( () => {
+    if (player.playState !== 'playing') {return;}
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( currentTime);
+    $('#time-control input').val(percent);
+  }, 1000);
+}
 
   $('button#previous').on('click', function(){
     if (player.playState !== 'playing') {return;}
@@ -23,6 +41,5 @@
     if (previousSongIndex >= album.songs.length) {return;}
 
     const previousSong = album.songs[previousSongIndex];
-    player.playPause(previousSong);
+    helper.playPauseAndUpdate(previousSong);
   });
-}
